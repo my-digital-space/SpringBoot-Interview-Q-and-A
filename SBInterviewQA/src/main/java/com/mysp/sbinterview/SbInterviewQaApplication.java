6b5fb3eedd4f1e432ca62c0326fd81289241d922
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-06-17 18:31:47 +0530

    initial commit

---
+++ SBInterviewQA/src/main/java/com/mysp/sbinterview/SbInterviewQaApplication.java
@@ -0,0 +1,13 @@
+package com.mysp.sbinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbInterviewQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbInterviewQaApplication.class, args);
+	}
+
+}
---
+++ SBInterviewQA/src/test/java/com/mysp/sbinterview/SbInterviewQaApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.mysp.sbinterview;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SbInterviewQaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
