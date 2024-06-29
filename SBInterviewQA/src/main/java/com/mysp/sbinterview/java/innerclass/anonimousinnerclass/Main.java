6aab572b55e3ac93ff6dd26dff8915dae71baf99
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-06-29 09:28:21 +0530

    java - inner anonymous class

--- SBInterviewQA/src/main/java/com/mysp/sbinterview/java/innerclass/anonimousinnerclass/Main.java
+++ SBInterviewQA/src/main/java/com/mysp/sbinterview/java/innerclass/anonimousinnerclass/Main.java
@@ -3,15 +3,28 @@ package com.mysp.sbinterview.java.innerclass.anonimousinnerclass;
 public class Main {
 
     public static void main(String[] args) {
-        Pizza pizza = new Pizza();
-        pizza.makePepperoniPizza();
+//        Pizza pepperoniPizza = new Pizza();
+//        pepperoniPizza.makeMyPizza();
 
-        Pizza MargheritaPizza = new Pizza(){
+        Pizza margheritaPizza = new Pizza() {
             // we just have to open and close a curly brace
             // this is where we will put our new class's definition.
-            // Here, we are creating an anonymous subclass of Pizza class.
-            
+            // Here, we are creating an anonymous subclass of Pizza class and overriding the method.
+            @Override
+            public void makeMyPizza() {
+                System.out.println("Making a Margherita Pizza!");
+            }
         };
+        //margheritaPizza.makeMyPizza();
+
+        Runnable myAnonymousRunnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello World from anonymous Runnable!");
+            }
+        };
+        myAnonymousRunnable.run();
+
     }
 
 }
--- SBInterviewQA/src/main/java/com/mysp/sbinterview/java/innerclass/anonimousinnerclass/Pizza.java
+++ SBInterviewQA/src/main/java/com/mysp/sbinterview/java/innerclass/anonimousinnerclass/Pizza.java
@@ -2,7 +2,7 @@ package com.mysp.sbinterview.java.innerclass.anonimousinnerclass;
 
 public class Pizza {
 
-    public void makePepperoniPizza() {
+    public void makeMyPizza() {
         System.out.println("Making a Pepperoni Pizza!");
     }
 
