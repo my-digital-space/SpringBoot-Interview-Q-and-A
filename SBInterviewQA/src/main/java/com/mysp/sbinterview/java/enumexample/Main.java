f5f6e23f109c9d0a66c84ce3fb3eb0bbb9139a36
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-06-29 13:19:35 +0530

    java - enum

---
+++ SBInterviewQA/src/main/java/com/mysp/sbinterview/java/enumexample/Main.java
@@ -0,0 +1,18 @@
+package com.mysp.sbinterview.java.enumexample;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        MonthsEnum month = MonthsEnum.JUNE;
+//        if(month == MonthsEnum.JUNE) {
+//            System.out.println("It's a half year already!");
+//        }
+//        for(MonthsEnum month: MonthsEnum.values()) {
+//            System.out.println(month);
+//        }
+        //MonthsEnum.JUNE.monthNumber = 13;
+        System.out.println(MonthsEnum.JUNE);
+        System.out.println(MonthsEnum.JUNE.monthNumber);
+    }
+
+}
---
+++ SBInterviewQA/src/main/java/com/mysp/sbinterview/java/enumexample/MonthsEnum.java
@@ -0,0 +1,15 @@
+package com.mysp.sbinterview.java.enumexample;
+
+public enum MonthsEnum {
+
+    JANUARY(1), FEBRUARY(2), MARCH(3), APRIL(4),
+    MAY(5), JUNE(6),
+    JULY(7), AUGUST(8), SEPTEMBER(9), OCTOBER(10),
+    NOVEMBER(11), DECEMBER(12);
+
+    final int monthNumber;
+
+    MonthsEnum(int monthNumber) {
+        this.monthNumber = monthNumber;
+    }
+}
